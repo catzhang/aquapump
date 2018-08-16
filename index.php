c697ba49fcf38e19f196c2d1d75bea1f78f47fcf
Cathy Zhang <cathystars11@gmail.com>
2018-08-16 15:05:09 -0700

    create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1>Salinity Sensor Data</h1>
+
+<?php
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>ID</th><th>timestamp</th><th>EC_salt</th><th>PPM_salt</th><th>temp_salt</th><th>EC_fresh</th><th>PPM_fresh</th><th>temp_fresh</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+
+$servername = "localhost";
+$username = "tencent18";
+$password = "tencent890";
+$dbname = "mydb";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM SalinityDB"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?> 
+
+</body>
+</html>
